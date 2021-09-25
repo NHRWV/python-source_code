5eed1de69479a3c247cea03de4fb37e804111b66
남현 <54979241+NHRWV@users.noreply.github.com>
2021-09-25 15:51:30 +0900

    Update 에라토스테네스의 체.py

--- 에라토스테네스의 체.py
+++ 에라토스테네스의 체.py
@@ -1,6 +1,11 @@
+# n값이 소수인지 확인하는 함수.
+
 import math
 
-def is_prime_num(n):  # n값이 소수인지 확인
+def is_prime_num(n):
+    if n == 1:
+        return 0
+    
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return 0
