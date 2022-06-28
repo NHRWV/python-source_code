711ab4269fec7ffeac6a76b13cf75d4d5267c38a
남현 <nhk221gu@gmail.com>
2022-06-28 14:53:51 +0900

    Create 세그먼트_트리.py

---
+++ 세그먼트_트리.py
@@ -0,0 +1,65 @@
+import sys
+# input = lambda: sys.stdin.readline().rstrip()
+
+inf = float('inf')
+
+class SegmentTree:
+    def __init__(self, data, default=10**15, func=lambda a, b: max(a,b)):
+        """initialize the segment tree with data"""
+        self._default = default
+        self._func = func
+        self._len = len(data)
+        self._size = _size = 1 << (self._len - 1).bit_length()
+ 
+        self.data = [default] * (2 * _size)
+        self.data[_size:_size + self._len] = data
+        for i in reversed(range(_size)):
+            self.data[i] = func(self.data[i + i], self.data[i + i + 1])
+ 
+    def __delitem__(self, idx):
+        self[idx] = self._default
+ 
+    def __getitem__(self, idx):
+        return self.data[idx + self._size]
+ 
+    def __setitem__(self, idx, value):
+        idx += self._size
+        self.data[idx] = value
+        idx >>= 1
+        while idx:
+            self.data[idx] = self._func(self.data[2 * idx], self.data[2 * idx + 1])
+            idx >>= 1
+ 
+    def __len__(self):
+        return self._len
+ 
+    def query(self, start, stop):
+        if start == stop:
+            return self.__getitem__(start)
+        stop += 1
+        start += self._size
+        stop += self._size
+ 
+        res = self._default
+        while start < stop:
+            if start & 1:
+                res = self._func(res, self.data[start])
+                start += 1
+            if stop & 1:
+                stop -= 1
+                res = self._func(res, self.data[stop])
+            start >>= 1
+            stop >>= 1
+        return res
+ 
+    def __repr__(self):
+        return "SegmentTree({0})".format(self.data)
+    
+n, m = map(int, input().split())
+li = [int(input()) for _ in range(n)]
+
+func = lambda a, b: a + b # 최댓값시 max(a, b)
+seg = SegmentTree(li, 기본 값 (덧셈시 0, 최댓값시: -inf, 최솟값시: inf, func = func)
+
+seg.query(구간 시작, 구간 끝)
+                  
